fc9154f46a20f7f8b924f2a844f6c2f0f25d952e
tbui17 <86227911+tbui17@users.noreply.github.com>
2023-01-13 06:07:51 -0700

    commit

--- printers_script/printers_script.py
+++ printers_script/printers_script.py
@@ -2,6 +2,7 @@ import re
 import pyautogui as pa
 import time
 import pyperclip
+import cv2
 
 #Vars
 data = """
@@ -16,16 +17,38 @@ Assignment_Group = 'Baltimore Support'
 # Coords_Short_Description = 
 # Coords_Save = 
 # Coords_Ticket_Number = 
+# Coords_Printers_Applied = 
+# Coords_Create_Security_Incident = 
+# Coords_Submit = 
 
 
-#DEBUG COORDINATES
-Coords_Create_New = (2701,615)
-Coords_Printers2 = (2701,615)
-Coords_Assignment_Group = (2701,615)
-Coords_Short_Description = (2701,615)
-Coords_Save = (2701,615)
-Coords_Ticket_Number = (1785,429)
+# DEBUG COORDINATES
+# Coords_Create_New = (2701,615)
+# Coords_Printers2 = (2701,615)
+# Coords_Assignment_Group = (2701,615)
+# Coords_Short_Description = (2701,615)
+# Coords_Save = (2701,615)
+# Coords_Ticket_Number = (1785,429)
+# Coords_Printers_Applied = (1785,429)
+# Coords_Create_Security_Incident = (1785,429)
+# Coords_Submit = (1785,429)
 
+#Locate On Screen Inputs
+Create_New = "Create_New"
+save = "save"
+printers2 = "printers2"
+
+#Pictures Paths
+Pic_Printers_Applied =  #Printers2 template applied. Checks if page has fully loaded after using the template.
+Pic_Create_Security_Incident =  #Checks page if it has finished saving.
+Pic_Submit =  #Checks if loading finished after creating new ticket.
+
+
+#Regions
+#You can pass a four-integer tuple of the left, top, width, and height of the region to capture
+create_new_region = 
+save_region = 
+printers2_region = 
 
 def regex(data):
     importedresults = data
@@ -36,6 +59,29 @@ def regex(data):
     print("Raw list: ")
     print(finalresult)
     return finalresult
+
+def locate_on_screen(data):
+    count = 0
+    while True:
+        print(f"Beginning locateOnScreen for {data}...")
+        if data is "Create_New":
+            res1 = pa.locateOnScreen(Pic_Submit, region=create_new_region, confidence=0.9)
+        elif data is "printers2":
+            res1 = pa.locateOnScreen(Pic_Printers_Applied, region=printers2_region, confidence=0.9)
+        elif data is "save":
+            res1 = pa.locateOnScreen(Pic_Create_Security_Incident, region=save_region, confidence=0.9)
+        else:
+            raise Exception("Invalid data provided for locateOnScreen")
+        if res1 is None:
+            if count == 3:
+                raise Exception("Tried 3 additional times, process still not complete or other error. See log.")
+            count+=1
+            print(f"Process not done yet. Retrying {3 - count} more times.")
+            print("Sleeping for 6 seconds...")
+            time.sleep(6)
+        else:
+            print("Found picture on screen.")
+            break
 
 def script1(data):
     def clear():
@@ -53,15 +99,23 @@ def script1(data):
             
     printer_name = data
     
-    #Create new ticket then use template
+    #Create new ticket
     pa.click(Coords_Create_New[0], Coords_Create_New[1])
     sleep1 = 3
     print(f"Clicked create new, now sleeping for {sleep1} seconds...")
     time.sleep((sleep1))
+    print("Checking if process has finished.")
+    locate_on_screen(Create_New)
+    print("Process finished.")
+    
+    #Use template
     pa.click(Coords_Printers2[0], Coords_Printers2[1])
     sleep2 = 2
     print(f"Clicked printer template, now sleeping for {sleep2} seconds...")
     time.sleep(sleep2)
+    print("Checking if process has finished.")
+    locate_on_screen(printers2)
+    print("Process finished.")
     
     #Add printer to beginning of short description
     pa.click(Coords_Short_Description[0], Coords_Short_Description[1])
@@ -84,7 +138,9 @@ def script1(data):
     sleep3 = 6
     print(f"Clicked save, now sleeping for {sleep3} seconds...")
     time.sleep(sleep3)
-    
+    print("Checking if process has finished.")
+    locate_on_screen(printers2)
+    print("Process finished.")
     
 
 
