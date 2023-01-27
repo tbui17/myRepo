0547eaa1ca5a18c28ba5b7d3105fe67e2cc83b2e
tbui17 <86227911+tbui17@users.noreply.github.com>
2023-01-27 05:52:00 -0700

    Update snowScripts.js

--- snowScripts.js
+++ snowScripts.js
@@ -27,7 +27,7 @@ class csPage {
 
 class incidentPage {
   // these buttons have a shadow root directly underneath. The only other way to differentiate them is their index number which means they may accidentally refer to a different button if their positions change
-  static createSecurityIncident = () =>
+  static createSecurityIncidentButton = () =>
     document.querySelector(
       "#rso > div:nth-child(2) > div > div > div > div:nth-child(2) > div"
     );
@@ -37,7 +37,7 @@ class incidentPage {
     );
 
   static moreActionsButton = () => {};
-  static composeEmail = () => {};
+  static composeEmailButton = () => {};
 
   // may be prone to breaking if search results return more than one template
   static templateButton = () => {};
@@ -77,7 +77,46 @@ class emailPage {
 class securityIncidentPage {}
 
 class oldCsPage {}
-class oldIncPage {}
+
+const oldIncPage = {
+  iframe: () => {},
+
+  numberField: () => {},
+  email: () => {},
+  businessPhone: () => {},
+  contractor: () => {},
+  preferredContactMethod: () => {},
+  category: () => {},
+  subcategory: () => {},
+  businessService: () => {},
+  configurationItem: () => {},
+  attachKba: () => {},
+
+  opened: () => {},
+  openedBy: () => {},
+  channel: () => {},
+  state: () => {},
+  impact: () => {},
+  urgency: () => {},
+  priority: () => {},
+  previousAssignmentGroup: () => {},
+  assignmentGroup: () => {},
+  assignedTo: () => {},
+
+  shortDescriptionField: () => {},
+  descriptionBox: () => {},
+  workNotesBox: () => {},
+
+  saveButton: () => {},
+  submitButton: () => {},
+  resolveButton: () => {},
+
+  templateButton: () => {},
+  templateSearchBar: () => {},
+  printers2Template: () => {},
+
+  alertWindow: () => {},
+}
 
 class oldEmailPage {}
 
@@ -153,7 +192,7 @@ class scripts {
   static async jsonGetInfo() {
     const output = await scripts.oldSearchGetInfo();
     const outputInJson = JSON.stringify(output, null, 4);
-    console.log(outputInJson);
+    console.debug(outputInJson);
   }
 
   static async notificationMail() {
@@ -162,9 +201,40 @@ class scripts {
     await scripts.activateTemplate(csPage, "notificationMail");
     await csPage.proposeSolutionsButton().click()
   }
+
+  
+  
+  static async printers(descContent, supportCenter){
+    const arrayMatches = scripts.regexEmail(descContent)
+    const arrayTicketNumbers = []
+    for (const match of arrayMatches) {
+      await waitForExist(oldIncPage.templateButton)
+      await sleep(500)
+      let ticketNumber = oldIncPage.ticketNumber().value
+      console.debug(`ticketNumber is ${ticketNumber}`)
+      arrayTicketNumbers.push(ticketNumber)
+      await scripts.activateTemplate(oldIncPage, "printers2")
+      setFieldValue(oldIncPage.assignmentGroup, `${supportCenter}`)
+      setFieldValue(oldIncPage.assignmentGroup, `${match} is now Stale`)
+      await sleep(500)
+      await scripts.savePage(oldIncPage)
+      await waitForExist(oldIncPage.createSecurityIncidentButton)
+      await sleep(500)
+      await waitForExist(oldIncPage.createIncidentButton)
+      await oldIncPage.createIncidentButton().click()
+    }
+    console.log('Complete. Ticket numbers below.')
+    console.log(`${arrayTicketNumbers}`)
+  }
   // mini scripts
 
   static async() {}
+
+  static regexPrinters(input) {
+    const regexSubject = /LEXMARK\s+\S+/g;
+    const arrayMatches = regexSubject.exec(input)
+    return arrayMatches;
+  }
 
   static regexEmail(input) {
     const regexSubject = /(?<=Subject:).*/;
@@ -226,18 +296,26 @@ class scripts {
     await sleep(1000);
     setFieldValue(page.templateSearchBar, `*${searchTerm}`);
     await sleep(500);
-    await waitForExist(page.firstTemplate);
-    await sleep(500);
-    await page.firstTemplate().click();
-    await waitForExist(page.undoButton);
-    await sleep(500)
+    if (page === oldIncPage) {
+      await waitForExist(page.printers2Template);
+      await sleep(500);
+      await page.printers2Template().click();
+      await waitForExist(page.windowAlert);
+      await sleep(500)
+    } else {
+      await waitForExist(page.firstTemplate);
+      await sleep(500);
+      await page.firstTemplate().click();
+      await waitForExist(page.undoButton);
+      await sleep(500)
+    }
   }
 
   static async getDescriptionText(page) {
     // get description data
     const descBoxContents = page.descriptionBox().value;
     const arrayEmail = scripts.regexEmail(descBoxContents);
-    console.log("(getDescriptionText) ", arrayEmail);
+    console.debug("(getDescriptionText) ", arrayEmail);
     return arrayEmail;
   }
 
@@ -265,7 +343,7 @@ class scripts {
   static async sendMail(page, arrayEmail, text) {
     // send email
     if (arrayEmail === 111) {
-      console.log(
+      console.debug(
         "(sendMail) Subject does not contain email. Cancelling send mail."
       );
       return;
@@ -319,7 +397,7 @@ class scripts {
     try {
       const arrayOfAssetTagValues = [];
       for (const rowWithPersonalComputer of arrayOfRowsWithPersonalComputer) {
-        console.log("(getAssetTags) Getting row:", rowWithPersonalComputer);
+        console.debug("(getAssetTags) Getting row:", rowWithPersonalComputer);
         const cellWithAssetTag =
           rowWithPersonalComputer.querySelector("td:nth-of-type(3)");
         // const cellWithAssetTag = rowWithPersonalComputer.querySelector('input')
@@ -329,8 +407,8 @@ class scripts {
       const stringOfAssetTagValues = `${arrayOfAssetTagValues}`;
       return stringOfAssetTagValues;
     } catch (error) {
-      console.log("(getAssetTags)error: ", error);
-      console.log(
+      console.debug("(getAssetTags)error: ", error);
+      console.debug(
         "(getAssetTags) No element containing the asset tag was found."
       );
       return "n/a";
@@ -351,32 +429,32 @@ class scripts {
     // get all fields in search information and append them to the list
     const arrayOldSearchPageValues = Object.entries(oldSearchPage);
 
-    console.log("(oldSearchGetInfo) Beginning waitForExist loop");
+    console.debug("(oldSearchGetInfo) Beginning waitForExist loop");
     for (const [key, value] of arrayOldSearchPageValues) {
       if (key === "assetRows") {
-        console.log(
+        console.debug(
           `(oldSearchGetInfo) Skipping over ${key} as specified in oldSearchGetInfo`
         );
         break;
       }
-      await console.log(`(oldSearchGetInfo) Checking if exists: ${key}`);
+      await console.debug(`(oldSearchGetInfo) Checking if exists: ${key}`);
       await waitForExist(value);
     }
 
-    console.log("(oldSearchGetInfo) All field elements exist.");
-    console.log("(oldSearchGetInfo) Now appending element values to array");
+    console.debug("(oldSearchGetInfo) All field elements exist.");
+    console.debug("(oldSearchGetInfo) Now appending element values to array");
 
     const arrayFieldValues = {};
 
-    console.log("(oldSearchGetInfo) Beginning append to list loop");
+    console.debug("(oldSearchGetInfo) Beginning append to list loop");
     for (const [key, value] of arrayOldSearchPageValues) {
       if (key === "assetRows") {
-        console.log(
+        console.debug(
           `(oldSearchGetInfo) Not appending and will skip over ${key} as specified in oldSearchGetInfo`
         );
         break;
       }
-      console.log(
+      console.debug(
         `(oldSearchGetInfo) Pushing into arrayFieldValues array: "${key}" element whose value is`,
         value().value
       );
@@ -386,7 +464,7 @@ class scripts {
     // appending asset tags to list
     let assetTags = scripts.getAssetTagsOldSearchPage();
     arrayFieldValues["Asset Tag"] = assetTags;
-    console.log(
+    console.debug(
       `(oldSearchGetInfo) For the key: "Asset Tag", the value ${assetTags} was pushed.`
     );
     return arrayFieldValues;
@@ -428,56 +506,56 @@ function elementValidation(page) {
     }
     if (valueActive === null) {
       console.groupCollapsed(`%c${key} was null`, "color: #27ACEF");
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     } else if (valueActive === "null (Shadow Root)") {
       console.groupCollapsed(
         `%c${key} was null (Shadow Root)`,
         "color: #E713A3"
       );
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     } else if (valueActive === undefined) {
       console.groupCollapsed(`%c${key} was undefined`, "color: #A020F0");
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     } else if (valueActive === false) {
       console.groupCollapsed(`%c${key} was false`, "color: #FF0000");
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     } else if (valueActive instanceof Element) {
       console.groupCollapsed(`%c${key} exists.`, "color: #168E2F");
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     } else {
       console.groupCollapsed(`%c${key} is unknown??.`, "color: #FFA500");
-      console.log(`key: ${key}`);
-      console.log(`value: ${value}`);
-      console.log(`valueActive: ${valueActive}`);
+      console.debug(`key: ${key}`);
+      console.debug(`value: ${value}`);
+      console.debug(`valueActive: ${valueActive}`);
       console.groupEnd();
     }
   }
 }
 
 function setFieldValue(selector, value) {
-  console.log("(setFieldValue fieldElement and value are)", selector, value);
+  console.debug("(setFieldValue fieldElement and value are)", selector, value);
   selector().focus();
   selector().select();
   document.execCommand("insertText", false, value);
 }
 
 function emailSetFieldValue(selector, value, parentDocumentSelector) {
-  console.log("(setFieldValue fieldElement and value are)", selector, value);
+  console.debug("(setFieldValue fieldElement and value are)", selector, value);
   selector().focus();
   selector().select();
   parentDocumentSelector().execCommand("insertText", false, value);
@@ -490,7 +568,7 @@ function queryCleaner(queryAsString) {
     /(#chrome-tab-panel-record_[\w\d]*)/,
     "[class='chrome-tab-panel is-active']"
   );
-  console.log(final);
+  console.debug(final);
 }
 
 const deepSelectorAll = (node, selector) => {
@@ -511,45 +589,45 @@ function sleep(ms) {
 
 function retry(fn, delay = 200, retries = 5, err = null) {
   if (!retries) {
-    console.log(`(Retry Function) Failed. Out of tries. Parameter was ${fn}`);
+    console.debug(`(Retry Function) Failed. Out of tries. Parameter was ${fn}`);
     return Promise.reject(err);
   }
-  console.log(`Beginning try for ${fn}`)
+  console.debug(`Beginning try for ${fn}`)
   return fn().catch(async (err) => {
     console.groupCollapsed(`(Retry Function) Failed. Tries remaining: ${retries}`);
     if (delay !== 0) {
       if (retries === 1) {
         delay += 2000;
-        console.log(
+        console.debug(
           "(Retry Function)Last retry, sleeping for 5000 ms longer than initial delay"
         );
       }
       if (retries === 2) {
         delay += 2000;
-        console.log(
+        console.debug(
           "(Retry Function)4th retry, sleeping for 3000 ms longer than initial delay"
         );
       }
       if (retries === 3) {
         delay += 500;
-        console.log(
+        console.debug(
           "(Retry Function) 3rd retry, sleeping for 1000 ms longer than initial delay"
         );
       }
       if (retries === 4) {
         delay += 500;
-        console.log(
+        console.debug(
           "(Retry Function) 2nd retry, sleeping for 500 ms longer than initial delay"
         );
       }
       if (retries === 5) {
-        console.log(
+        console.debug(
           "(Retry Function) 1st retry, sleeping for specified delay."
         );
       }
-      console.log(`(Retry Function) Sleeping for ${delay} ms...`);
+      console.debug(`(Retry Function) Sleeping for ${delay} ms...`);
       await sleep(delay);
-      console.log("(Retry Function) Done sleeping.");
+      console.debug("(Retry Function) Done sleeping.");
     }
     console.groupEnd()
     return retry(fn, delay, retries - 1, err);
@@ -557,25 +635,25 @@ function retry(fn, delay = 200, retries = 5, err = null) {
 }
 async function errorCheck(funct) {
   try {
-    console.log(`(errorCheck) 1st check. The key for funct is ${funct.name}. Will now perform const elem = funct().`)
+    console.debug(`(errorCheck) 1st check. The key for funct is ${funct.name}. Will now perform const elem = funct().`)
     const elem = funct();
-    console.log(
+    console.debug(
       "(errorCheck) funct() works. The values of elem and funct are:",
       { elem },
       { funct }
     );
-    console.log("(errorCheck) 2nd check for elem === null or undefined.");
+    console.debug("(errorCheck) 2nd check for elem === null or undefined.");
     if (elem === undefined || elem === null) {
-      console.log("(ErrorCheck) (1/3) Failed. The parameter is listed below.");
-      console.log("(ErrorCheck) (2/3) elem: ", elem);
-      console.log("(ErrorCheck) (3/3) funct:", funct);
+      console.debug("(ErrorCheck) (1/3) Failed. The parameter is listed below.");
+      console.debug("(ErrorCheck) (2/3) elem: ", elem);
+      console.debug("(ErrorCheck) (3/3) funct:", funct);
       throw new Error(
         `(errorCheck) The elem is Null/Undefined. The elem is: ${elem}`
       );
     }
-    console.log('(ErrorCheck) Success, elem and funct are: ', elem, funct);
+    console.debug('(ErrorCheck) Success, elem and funct are: ', elem, funct);
   } catch (error) {
-    console.log(`(ErrorCheck) There was an error, which is: ${error}`);
+    console.debug(`(ErrorCheck) There was an error, which is: ${error}`);
     throw new Error("");
   }
 }
