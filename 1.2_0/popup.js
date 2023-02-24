4f7bc5c352b516f95f45e7f103675f5ecb994af2
tbui17 <86227911+tbui17@users.noreply.github.com>
2023-02-24 03:39:33 -0700

    update1

--- 1.2_0/popup.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById("genid");
-
-btn.addEventListener("click", function(){   
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
-    document.write("<div style='text-align:center; margin-top:100px;'><h2 style='color:green';>Great !! Now just click on the xpath to copy.</h4></div>");
-  });
-});
